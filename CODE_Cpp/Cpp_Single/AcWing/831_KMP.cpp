81ca5963e48eda0470723411fdda4d162daca16f
LywockeeZ <kofln@vip.qq.com>
2020-09-06 01:41:07 +0800

    update

--- CODE_Cpp/Cpp_Single/AcWing/831_KMP.cpp
+++ CODE_Cpp/Cpp_Single/AcWing/831_KMP.cpp
@@ -1,28 +1,29 @@
 #include<iostream>
 using namespace std;
 
-const int N = 1e6 + 10;
-char p[N], s[N], ne[N];
-int n, m;
+const int N = 1e5 + 10, M = 1e6 + 10;
+char p[N], s[M];
+int n, m, ne[N];
 
 int main()
 {
     cin >> n >> p + 1 >> m >> s + 1;
 
-    for(int i = 2, j = 0; i <= m; i++)
+    for(int i = 2, j = 0; i <= n; i++)
     {
         while(j && p[i] != p[j + 1]) j = ne[j];
-        if(p[i] == s[j + 1]) j++;
+        if(p[i] == p[j + 1]) j++;
         ne[i] = j;
     }
 
-    for(int i = 1, j = 0; i <= n; i++)
+    for(int i = 1, j = 0; i <= m; i++)
     {
-        while(j && p[i] != s[j + 1]) j = ne[j];
-        if(p[i] == s[j + 1]) j++;
-        if(j == m) 
+        while(j && s[i] != p[j + 1]) j = ne[j];
+        if(s[i] == p[j + 1]) j++;
+        if(j == n) 
         {
-            cout << i - m + 1 << " ";
+            j = ne[j];
+            cout << i - n << " ";
         }
     }
 
